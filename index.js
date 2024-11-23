7a4198c7a51b1d48fb0173a260baa0463e3fa66f
Priyanshu Omer <146474281+Priyanshuomer@users.noreply.github.com>
2024-11-23 19:01:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+const {Client,GatewayIntentBits} = require("discord.js");
+
+const client = new Client({
+    intents:[GatewayIntentBits.Guilds,GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent]
+});
+
+client.on("messageCreate", (message) => {
+    if(message.author.bot) return ;
+
+    message.reply({
+        content:"hey from bot"
+    });
+});
+
+client.on("interactionCreate",(interaction) => {
+    console.log(interaction);
+    interaction.reply("helloo interactions !!!")
+});
+
+client.login("###");
+
+
+
+
+
+
+
+
+
+
+
 
